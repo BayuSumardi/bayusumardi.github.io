4c556fe912b0c41bef83f5953f300caa28da8ffc
fikri fikri <davaarvarabi9@gmail.com>
2026-06-27 13:26:38 +0700

    Fix: Navigasi Analytics, Dropdown tipe data, dan perbaikan URL slash

--- vigenesiaV2/flutter_service_worker.js
+++ vigenesiaV2/flutter_service_worker.js
@@ -24,15 +24,15 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "d5a3921bfd39302be08a7e067a55b0d3",
+"flutter_bootstrap.js": "fa74700c0bda429c1cee85068d317245",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "09b9053891d34e7784d472daa319bb61",
-"/": "09b9053891d34e7784d472daa319bb61",
+"index.html": "506bc5fe08834a104a607bb6567f8cc7",
+"/": "506bc5fe08834a104a607bb6567f8cc7",
 "main.dart.js": "04e14a3bc287791894012bf28db90fa3",
-"manifest.json": "2c1ef8e8c9c941903057efa0b2205471",
+"manifest.json": "ea0807c1f71bdaae5b738f7b00f3e385",
 "version.json": "bf7ade1b47b08ea56524e415eb65d06c"};
 // The application shell files that are downloaded before a service worker can
 // start.
